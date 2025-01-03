802e0882c2325a44dd3f1ae817c9096d530def42
omer-zaadi <omerzadi2014@gmail.com>
2025-01-03 11:53:19 +0200

    Add files via upload

---
+++ Cheaker2/Piece.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheaker2
+{
+    public class Piece
+    {
+        public char Symbol { get; private set; } // X או O
+        public bool IsKing { get; private set; }
+        public string Owner { get; private set; } // שם הבעלים של הכלי
+
+        public Piece(char symbol, string owner)
+        {
+            Symbol = symbol;
+            IsKing = false;
+            Owner = owner;
+        }
+
+        public void PromoteToKing()
+        {
+            IsKing = true;
+            Symbol = char.ToUpper(Symbol); // X או O הופך ל-X או O באות גדולה
+        }
+    }
+
+}
